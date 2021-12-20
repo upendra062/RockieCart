217f8c8a808a17fd4238ba38867f3db4e02384c9
Upendra Singh Bhadauria <64073812+upendra062@users.noreply.github.com>
2021-12-20 23:22:21 +0530

    Revert "Master"

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
 from . import views
-
 urlpatterns = [
-    path("", views.index,name="blogHome"),
-    path("blogpost/<int:id>", views.blogpost,name="blogHome")
+    path('', views.index,name="blogHome"),
 ]
--- shop/urls.py
+++ shop/urls.py
@@ -10,5 +10,4 @@ urlpatterns = [
     path("products/<int:myid>", views.productView, name="ProductView"),
     path("checkout/", views.checkout, name="Checkout"),
     path("rockie/", views.rockie, name="Rockie"),
-    path("handlerequest/", views.handlerequest, name="Handlerequest"),
 ]
